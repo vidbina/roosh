2b9d8f39783e09377cbb47c79eb93045737d2041
David Asabina <david@supr.nu>
2013-10-07 23:54:01 +0200

    Something to start off with... let's figure out testing first

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send('Roosh!');
+});
+
+app.get('/restrooms', function(req, res) {
+  // TODO: return list of restrooms
+});
+
+app.put('/restrooms', function(req, res) {
+  // TODO: update restroom data, like ratings
+});
+
+app.post('/restrooms', function(req, res) {
+  // TODO: add new restroom
+});
+
+app.delete('/restrooms', function(req, res) {
+  // TODO: remove restroom
+});
+
+app.listen(4343);
